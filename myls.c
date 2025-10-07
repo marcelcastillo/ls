53fca881beb2db34ce65ffab48a467555c97d36c
Marcel Castillo <mcastill1@stevens.edu>
2025-10-07 12:41:33 -0400

    Printing local children filenames

--- myls.c
+++ myls.c
@@ -11,6 +11,7 @@ main(int argc, char *argv[])
 {
 	FTS *ftsp;
 	FTSENT *ent;
+	FTSENT *child;
 
 
 	if (argc < 2){
@@ -18,31 +19,40 @@ main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	/* Open FTS non-recursively */
+	/* Open FTS on the provided path */
 	ftsp = fts_open(argv + 1, FTS_PHYSICAL | FTS_NOCHDIR, NULL);
 	if (ftsp == NULL){
 		perror("fts_open");
 		exit(EXIT_FAILURE);
 	}
+	
+	/* Move to the first (root) node */
+	ent = fts_read(ftsp);
+	if (ent == NULL){
+		perror("fts_read");
+		fts_close(ftsp);
+		exit(EXIT_FAILURE);
+	}
 
-    // iterate through all roots
-    while ((ent = fts_read(ftsp)) != NULL) {
-        if (ent->fts_info == FTS_D) {  // only act on directories
-            printf("%s:\n", ent->fts_path);
+	// Make sure argument is a directory
+	if (ent->fts_info != FTS_D){
+		fprintf(stderr, "%s is not a directory\n", argv[1]);
+		fts_close(ftsp);
+		exit(EXIT_FAILURE);
+	}
 
-            FTSENT *child = fts_children(ftsp, 0);
-            if (!child) {
-                perror("fts_children");
-                continue;
-            }
+	// Get immediate children of this directory
+	child = fts_children(ftsp, 0);
+	if (child == NULL){
+		perror("fts_children");
+		fts_close(ftsp);
+		exit(EXIT_FAILURE);
+	}
 
-            for (FTSENT *p = child; p; p = p->fts_link)
-                printf("  %s\n", p->fts_name);
-
-            printf("\n");
-        }
+	// Print only the child names
+    for (FTSENT *p = child; p != NULL; p = p->fts_link) {
+        printf("%s\n", p->fts_name);
     }
-
 
 
 	fts_close(ftsp);
