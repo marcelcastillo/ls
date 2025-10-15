e3d78e7c27c50709ea4a52f6dc4510fc2be9354e
Marcel Castillo <mcastill1@stevens.edu>
2025-10-15 14:11:36 -0400

    Added optind functionality to skip past flags

---
+++ README.md
@@ -0,0 +1,7 @@
+Marcel Castillo
+Mon 13 Oct 2025
+Initial prototype for the ls midterm assignment
+
+- Significant inspiration for the code was drawn from the ls.c NetBSD binary,
+    specifically the control flow around the fts family of directory tree
+    traversal functions
--- myls.c
+++ myls.c
@@ -21,14 +21,18 @@ main(int argc, char *argv[])
 	FTSENT *child;	
 	int ch;
 	int fts_options;
-	//static char dot[] = ".";
+	static char dot[] = ".";
 
-	int f_listdot = 0;		/* */
-	int f_nosort = 0;
+	int f_listdot = 	0;		/* */
+	int f_nosort = 		0;
+	int f_recursive = 	0;
 
-	fts_options = FTS_PHYSICAL;		// Option bit-mask
+	fts_options = FTS_PHYSICAL;		// Option bit-mask, start by not following symbolic links
 	while ((ch = getopt(argc, argv, "-AacdFfhiklnqRrSstuw")) != -1){
 		switch (ch){
+			case 'R':
+				f_recursive = 1;
+				break;
 			case 'f':
 				f_nosort = 1;
 				/* FALLTHROUGH */
@@ -37,36 +41,38 @@ main(int argc, char *argv[])
 				/* FALLTHROUGH */
 			case 'A':
 				f_listdot = 1;				// Lists all entries except for '.' and '..'
-				break;
+				break;						// Logical flag that is used 
+			case '?':
+				fprintf(stdout, "usage: %s [-AacdFfhiklnqRrSstuw] [file ...]\n", argv[0]);
+				exit(EXIT_FAILURE);
 		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (argc == 0){
+		argv[0] = dot;
 	}
 
 	/* -A is always set for the superuser */
-	if (!getuid()){
+	if (getuid() == 0){
 		f_listdot = 1;
 	}
 
-	if (argc < 2){
-		fprintf(stderr, "usage: %s [-AacdFfhiklnqRrSstuw] [file ...]\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-
 	/* Open FTS on the provided path */
-	/*						*/
-	ftsp = fts_open(argv + 1, fts_options | FTS_NOCHDIR, NULL);
+	ftsp = fts_open(argv, fts_options | FTS_NOCHDIR, NULL);
 	if (ftsp == NULL){
 		perror("fts_open");
 		exit(EXIT_FAILURE);
 	}
 	
 	/* Traverse through the fts tree abstraction */
-	while ((p = fts_read(ftsp)) != NULL)
+	while ((p = fts_read(ftsp)) != NULL)						// Begins with the target of ls
 		switch (p->fts_info){
 			case FTS_D:
-				/* Do not traverse into hidden directories (-a or -A are set) */
+				/* Skip '.' prefix files when -a or -A are set */
 				if (p->fts_level != FTS_ROOTLEVEL &&
-					p->fts_name[0] == '.' &&
-					!f_listdot)
+					p->fts_name[0] == '.' && f_listdot == 0)
 					{
 						(void)fts_set(ftsp, p, FTS_SKIP);
 						break;
@@ -81,17 +87,19 @@ main(int argc, char *argv[])
 				/* Print children */
 				/* If operating regularly (non -a or -A), hide hiddens */
 				for (FTSENT *c = child; c != NULL; c = c->fts_link){
-					if (!f_listdot && c->fts_name[0] == '.'){
+					if (f_listdot == 0 && c->fts_name[0] == '.'){
 						continue;
 					}
 					printf("	%s\n", c->fts_name);
 				}
-				fts_set(ftsp, p, FTS_SKIP);
-				break;
+				if (f_recursive == 0){
+					fts_set(ftsp, p, FTS_SKIP);
+					break;
+				}
 		}
 	if (f_nosort){
 		printf("lemons\n");
-	}
+	}	
 	fts_close(ftsp);
 	exit(EXIT_SUCCESS);
 }
